2a3522e343c107d560432d59d46c29b7204de8cb
Aisiahhh <aisantiago@gbox.adnu.edu.ph>
2024-07-18 12:32:32 +0800

    Calculator

---
+++ script.js
@@ -0,0 +1,17 @@
+function appendToResult(value) {
+    document.getElementById('result').value += value;
+}
+
+function clearResult() {
+    document.getElementById('result').value = '';
+}
+
+function calculate() {
+    let result = document.getElementById('result').value;
+    try {
+        let output = eval(result);
+        document.getElementById('result').value = output;
+    } catch (e) {
+        document.getElementById('result').value = 'Error';
+    }
+}
